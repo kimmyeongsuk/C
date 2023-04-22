7b5903bbe32cb4138b33fe3c95d4500b0cb8c018
김명석 <128604364+kimmyeongsuk@users.noreply.github.com>
2023-04-22 16:02:22 +0900

    Add files via upload

---
+++ 6슬라이드/6slide52.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int answer = rand()%100;
+    int guess;
+    int tries = 0;
+
+    do{
+        printf("정답을 추측하시오:");
+        scanf("%d", &guess);
+        tries++;
+        if( answer < guess)
+        {
+            printf("LOW\n");
+        }
+        if( answer > guess)
+        {
+            printf("HIGH\n");
+        }
+    }while (guess != answer);
+
+    printf("축하합니다.시도횟수=%d", tries);
+    return 0;
+}
