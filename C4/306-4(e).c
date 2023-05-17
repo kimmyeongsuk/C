7562e7b9ee6ae6cb1b01bf2d7bcfd2414408bc30
김명석 <128604364+kimmyeongsuk@users.noreply.github.com>
2023-05-17 20:26:32 +0900

    Delete C4 directory

--- C4/306-4(e).c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int i = 0x10;
-    for( ; i ; i>>= 1)
-        printf("Hello\n");
-}
--- C4/hello.c
+++
@@ -1,9 +0,0 @@
-/*주석 달기
-hello World*/
-#include <stdio.h>
-
-int main(void){
-
-    printf(" Hello World!");
-
-}
